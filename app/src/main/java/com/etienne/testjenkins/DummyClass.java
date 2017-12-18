3576fef6554ee080fe81791c0ab4089b6b384d83
Etienne Valette <evalette@mobiquityinc.com>
2017-12-18 11:24:35 +0100

    added test for code coverage

---
+++ app/src/main/java/com/etienne/testjenkins/DummyClass.java
@@ -0,0 +1,12 @@
+package com.etienne.testjenkins;
+
+/**
+ * Created by etiennevalette on 18/12/2017.
+ */
+
+public class DummyClass {
+
+    public int dummyMethod() {
+        return 4;
+    }
+}
---
+++ app/src/test/java/com/etienne/testjenkins/DummyClassTest.java
@@ -0,0 +1,24 @@
+package com.etienne.testjenkins;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by etiennevalette on 18/12/2017.
+ */
+public class DummyClassTest {
+
+    private DummyClass subject;
+
+    @Before
+    public void setUp() throws Exception {
+        subject = new DummyClass();
+    }
+
+    @Test
+    public void dummyMethodShouldReturnFour() throws Exception {
+        assertEquals(4, subject.dummyMethod());
+    }
+}
